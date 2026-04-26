dba72a9fa1607e2244c1c2afd1efb6eaec8c74a7
Simon Schwitz <simon@calucon.de>
2026-04-26 13:00:35 +0300

    feat: init spring boot project

---
+++ backend/profile-service/src/test/java/de/calucon/profile/ProfileSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package de.calucon.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

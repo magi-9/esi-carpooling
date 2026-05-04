1af6e178785a534d786e29391035f9d0eb1660e4
Simon Schwitz <simon@calucon.de>
2026-05-04 12:27:55 +0300

    feat(profile): integrate into main stack

--- backend/profile-service/src/main/java/de/calucon/esi/profile/repository/UserProfileRepository.java
+++ backend/profile-service/src/main/java/de/calucon/esi/profile/repository/UserProfileRepository.java
@@ -3,10 +3,8 @@ package de.calucon.esi.profile.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import de.calucon.esi.profile.entity.UserProfile;
 
-@Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
 }
--- backend/profile-service/src/main/java/de/calucon/esi/profile/repository/VehicleRepository.java
+++ backend/profile-service/src/main/java/de/calucon/esi/profile/repository/VehicleRepository.java
@@ -6,11 +6,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import de.calucon.esi.profile.entity.Vehicle;
 
-@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
 
     @Query("SELECT v FROM Vehicle v WHERE v.userProfile.userId = :userId AND v.isVerified = true")
